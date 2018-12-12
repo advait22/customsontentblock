d0331d2b2a0b014b564906f3ce73558fa442ef34
Advait Deshpande <Advait.Deshpande@extentia.com>
2018-12-12 15:21:58 +0530

    added files

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(port, () => { console.log(`App listening on port ${port}`) });
