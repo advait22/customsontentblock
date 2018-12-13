c633c57b361229e40e150e29c9739c309c6c6ff5
Advait Mangesh Deshpande <deshpandeadvait88@gmail.com>
2018-12-13 12:41:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
-
+var SDK = require('blocksdk');
+var sdk = new SDK();
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/index.html');
+    console.log(sdk);
 })
 
 app.listen(port, () => { console.log(`App listening on port ${port}`) });
